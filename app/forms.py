46b01ee97ae5e4c4cc24ed5e9213306723e5f0ce
jadawltrs <jada.walters@mymona.uwi.edu>
2025-03-16 23:56:30 -0500

    working on exercise 5

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, FileField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileRequired, FileAllowed 
 
 
 class LoginForm(FlaskForm):
